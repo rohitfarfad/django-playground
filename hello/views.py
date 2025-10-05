45fe2440ef9e787569c18d0f06e75e6e317deda5
Rohit Farfad <rohitfarfad@mac.mynetworksettings.com>
2025-10-05 02:21:34 -0400

    First Commit

---
+++ hello/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, "hello/index.html")
+def rohit(request):
+    return HttpResponse("Hello Rohit")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name":name.capitalize()
+    })
