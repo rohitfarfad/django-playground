dd7d8d52742b0069b6157c8ead5f7c3a992f2842
Rohit Farfad <rohitfarfad@mac.mynetworksettings.com>
2025-10-07 03:45:40 -0400

    Added configs

--- playground1/settings.py
+++ playground1/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'playground1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'ebroot',
+        'PASSWORD': 'HCaeurWwsXf03Bn',
+        'HOST': 'django-playground-db.csjugu6uiakv.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+
     }
 }
 
